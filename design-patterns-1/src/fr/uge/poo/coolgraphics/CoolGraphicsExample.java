be55a3f22054837f0034beee2b539b7ebb5d285f
Taeith <nils.pernet@gmail.com>
2020-10-06 18:24:29 +0200

    add coolgraphics library

---
+++ design-patterns-1/src/fr/uge/poo/coolgraphics/CoolGraphicsExample.java
@@ -0,0 +1,24 @@
+package fr.uge.poo.coolgraphics;
+
+import fr.uge.poo.coolgraphics.CoolGraphics.ColorPlus;
+
+public class CoolGraphicsExample {
+
+    public static void main(String[] args) {
+            CoolGraphics area=new CoolGraphics("Example",800,600);
+            area.repaint(ColorPlus.WHITE);
+            // il n'y a pas de mÃ©thode pour tracer un rectangle, il faut le faire Ã  la main
+            area.drawLine(100,20,140, 20, ColorPlus.BLACK);
+            area.drawLine(100,160,140, 160, ColorPlus.BLACK);
+            area.drawLine(100,20,100, 160, ColorPlus.BLACK);
+            area.drawLine(140,20,140, 160, ColorPlus.BLACK);
+
+            area.drawLine(100, 20, 140, 160, ColorPlus.BLACK);
+            area.drawLine(100, 160, 140, 20, ColorPlus.BLACK);
+
+            area.drawEllipse(100, 20, 40, 140,ColorPlus.BLACK);
+
+            area.waitForMouseEvents((x,y) -> System.out.println("Click on ("+x+","+y+")"));
+    }
+
+}

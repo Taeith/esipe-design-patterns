00f53713a40bbcb1b273bfdbb227597aa4227f26
Taeith <nils.pernet@gmail.com>
2020-10-06 11:13:59 +0200

    develop abstract class

--- design-patterns-1/src/fr/uge/poo/paint/ex5/Ellipse.java
+++ design-patterns-1/src/fr/uge/poo/paint/ex5/Ellipse.java
@@ -2,18 +2,10 @@ package fr.uge.poo.paint.ex5;
 
 import java.awt.Graphics2D;
 
-public class Ellipse implements Figure {
-	
-	int x;
-	int y;
-	int width;
-	int height;
+public class Ellipse extends AbstractFigure {
 	
 	public Ellipse(int x, int y, int width, int height) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
+		super(x, y, width, height);
 	}
 
 	@Override
@@ -22,17 +14,8 @@ public class Ellipse implements Figure {
 	}
 
 	@Override
-	public double distanceFrom(int x, int y) {
-		int xA = x;
-		int yA = y;
-		int xB = this.x + (width / 2);
-		int yB = this.y + (height / 2);
-		return Math.sqrt((xB - xA)*(xB - xA) + (yB - yA)*(yB - yA));
-	}
-
-	@Override
 	public String toString() {
-		return "Ellipse [x=" + x + ", y=" + y + ", width=" + width + ", height=" + height + "]";
+		return "Ellipse " + super.toString();
 	}
 	
 }
--- design-patterns-1/src/fr/uge/poo/paint/ex5/Rectangle.java
+++ design-patterns-1/src/fr/uge/poo/paint/ex5/Rectangle.java
@@ -2,37 +2,20 @@ package fr.uge.poo.paint.ex5;
 
 import java.awt.Graphics2D;
 
-public class Rectangle implements Figure {
-	
-	int x;
-	int y;
-	int width;
-	int height;
+public class Rectangle extends AbstractFigure {
 	
 	public Rectangle(int x, int y, int width, int height) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
+		super(x, y, width, height);
 	}
-
+	
 	@Override
 	public void draw(Graphics2D graphics) {
-		graphics.drawRect(x, y, width, height);		
+		graphics.drawRect(this.x, this.y, this.width, this.height);		
 	}
-
-	@Override
-	public double distanceFrom(int x, int y) {
-		int xA = x;
-		int yA = y;
-		int xB = this.x + (width / 2);
-		int yB = this.y + (height / 2);
-		return Math.sqrt((xB - xA)*(xB - xA) + (yB - yA)*(yB - yA));
-	}
-
+	
 	@Override
 	public String toString() {
-		return "Rectangle [x=" + x + ", y=" + y + ", width=" + width + ", height=" + height + "]";
-	}	
+		return "Rectangle " + super.toString();
+	}		
 	
 }
